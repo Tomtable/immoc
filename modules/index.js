171cf14c8362eb12736309391aa8d3967fef5734
啵啵小奶嘴 <406922351@qq.com>
2016-04-03 01:30:15 +0800

    初始版本

---
+++ modules/index.js
@@ -0,0 +1,10 @@
+var calss = require('./class')
+
+exports.add = function(classes) {
+	classes.forEach(function(item, index){
+		var _class = item
+		var teacherName = item.teacherName
+		var students = item.students
+	})
+	calss.add(teacherName, students)
+} 
